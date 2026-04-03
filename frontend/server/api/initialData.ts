a83bad204a4447ef2d322ed5b11ba3853ed21ecd
Thomas Prangenberg <t@thomas-prangenberg.de>
2026-04-03 09:47:06 +0200

    [Cleanup] delete experimental server endpoints

--- frontend/server/api/initialData.ts
+++
@@ -1,7 +0,0 @@
-/*
-    Experiment: if using a proxy api endpoint, will cookies be sent via ssr?
- */
-
-export default defineEventHandler((event) => {
-    return event.$fetch('https://api.typo3.ddev.site/?type=834')
-})
